937312a1db1c8ef925e7ea457d1a01365628e547
choichoijin <cuj10594@naver.com>
2022-05-13 01:34:54 +0900

    feat: ✨ 카카오 로컬 api 사용

--- week4/src/App.js
+++ week4/src/App.js
@@ -1,11 +1,48 @@
+import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 import GlobalStyle from './globalStyle.js';
 
 function App() {
-  
+  const [storeList, setStoreList] = useState([]);
+  const searchRef = useRef();
+
+  // 보여줄 가게 정보 가져오기.
+  const getStore = async () => {
+    setStoreList();
+  }
+
+  async function 특정지역떡볶이집가져오기(location) {
+    const result = await axios.get(
+      "https://dapi.kakao.com/v2/local/search/keyword",
+      {
+        headers: {
+          Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_AK}`,
+        },
+        params: {
+          query: location + " " + '떡볶이',
+        }
+      }
+    )
+    setStoreList(result.data.documents);
+  }
+
+
+
+  // 제출 버튼 핸들링. 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (searchRef.current) {
+      const regionInput = searchRef.current;
+      특정지역떡볶이집가져오기(regionInput.value);
+    }
   }
+
+  //첫 렌더링 후 가게 정보 불러오기.
+  useEffect(() => {
+    getStore();
+  }, []);
 
   return (
     <>
@@ -17,7 +54,7 @@ function App() {
           <LocationBased>▶︎ 지역 기반으로 검색할게요 <input type = "checkbox"></input></LocationBased>
           우리 동네는 여기에요
           <form onSubmit={(e) => handleSubmit(e)}>
-            <input type = "text" placeholder = "지역이름을 입력하세요"></input>
+            <input ref={searchRef} type = "text" placeholder = "지역이름을 입력하세요" />
             <button type = "submit">검색하기!</button>
           </form>
         </BaseInfo>
