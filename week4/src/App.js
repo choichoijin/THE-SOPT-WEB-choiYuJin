d94f20f21ff0f03f338fff5e0f7694a29038dc4b
choichoijin <cuj10594@naver.com>
2022-05-12 13:35:01 +0900

    refactor: 🔨 검색 기능 폼으로 수정

--- week4/src/App.js
+++ week4/src/App.js
@@ -2,22 +2,29 @@ import styled from 'styled-components';
 import GlobalStyle from './globalStyle.js';
 
 function App() {
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <>
       <GlobalStyle />
       <Container>
-      <Title>우리 동네 떡볶이집</Title>
-      <hr></hr>
-      <Search>
-        <LocationBased>지역 기반으로 검색할게요 <input type = "checkbox"></input></LocationBased>
-        우리 동네는 여기에요
-        <input type = "text" placeholder = "지역이름을 입력하세요"></input>
-        <SearchBtn>검색하기!</SearchBtn>
-      </Search>
-      <hr></hr>
-      <List>
+        <Title>우리 동네 떡볶이집</Title>
+        <hr></hr>
+        <BaseInfo>
+          <LocationBased>▶︎ 지역 기반으로 검색할게요 <input type = "checkbox"></input></LocationBased>
+          우리 동네는 여기에요
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input type = "text" placeholder = "지역이름을 입력하세요"></input>
+            <button type = "submit">검색하기!</button>
+          </form>
+        </BaseInfo>
+        <hr></hr>
+        <List>
 
-      </List>
+        </List>
       </Container>
     </>
   );
@@ -40,24 +47,25 @@ const Title = styled.h1`
   font-weight: 900;
 `;
 
-const Search = styled.div`
+const BaseInfo = styled.div`
   text-align: center;
-  font-size: 20px;
-  & > input {
-    display: block;
+  font-size: 25px;
+  & > form {
     margin: 10px auto;
+  }
+  & > form > input {
+    font-family: 'EarlyFontDiary';
+    margin: 0 5px;
+  }
+  & > form > button {
+    font-family: 'EarlyFontDiary';
   }
 `;
 
 const LocationBased = styled.p`
   display: block;
   margin: 30px 0;
-`;
-
-const SearchBtn = styled.button`
-  display: block;
-  margin: 0 auto;
-  font-family: 'EarlyFontDiary'
+  font-size: 20px;
 `;
 
 const List = styled.ul`
